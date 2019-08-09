c8f215bc1d94f5c684ea28b10bd34e0adeb2ac6e
Grisilda <46270641+Grisilda@users.noreply.github.com>
2019-08-09 10:05:38 +0200

    Add files via upload

---
+++ DockerProject/src/main/java/project/docker/DockerProjectApplication.java
@@ -0,0 +1,13 @@
+package project.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerProjectApplication.class, args);
+	}
+
+}
